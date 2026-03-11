dccd68cc643a4211a87f6b6199ee228bcf9a79fb
EvgeniGenchev <genajok@gmail.com>
2026-03-11 20:09:03 +0100

    (feat): name templating for projects

--- omurtag/commands.py
+++ omurtag/commands.py
@@ -5,34 +5,49 @@ from argparse import Namespace
 from os import listdir
 import shutil
 from shutil import copytree, rmtree
-from .utils import get_data_directory, create_directory
+from .utils import (
+    get_data_directory,
+    create_directory,
+    replace_in_files,
+)
+
 
 def run(args: Namespace):
     data_directory = _ensure_data_directory()
 
     match args.mode:
-        case 'add':
+        case "add":
             _add(args, data_dir=data_directory)
-        case 'list':
+        case "list":
             _list(args, data_dir=data_directory)
-        case 'create':
+        case "create":
             _create(args, data_dir=data_directory)
-        case 'remove':
+        case "remove":
             _delete(args, data_dir=data_directory)
-        case 'pull':
-            print("[yellow]'pull' is not yet implemented, coming soon![/yellow]")
+        case "pull":
+            print(
+                "[yellow]'pull' is not yet implemented, coming soon![/yellow]"
+            )
+        case "sync":
+            print(
+                "[yellow]'sync' is not yet implemented, coming soon![/yellow]"
+            )
         case _:
             print(args.mode)
-            assert False # this should never happen if argparse works
+            assert False  # this should never happen if argparse works
+
 
 def _ensure_data_directory() -> str:
     try:
         return get_data_directory()
     except FileNotFoundError:
-        return create_directory() #this can raise but idk 
+        return create_directory()  # this can raise but idk
 
-def _ensure_template_exists(args, data_dir, template:str, print_flag=True)-> bool:
-    if template not in (templates:=_list(args, data_dir, print_flag=False)):
+
+def _ensure_template_exists(
+    args, data_dir, template: str, print_flag=True
+) -> bool:
+    if template not in (templates := _list(args, data_dir, print_flag=False)):
         if print_flag:
             print("[red]Template not found![/red]")
             print("[orange1]Available templates:[/orange1]")
@@ -46,9 +61,9 @@ def _delete(args, data_dir):
 
     to_be_deleted = args.template_name
 
-    if not _ensure_template_exists(args, data_dir,
-                                  template=to_be_deleted,
-                                  print_flag=True):
+    if not _ensure_template_exists(
+        args, data_dir, template=to_be_deleted, print_flag=True
+    ):
         return
 
     try:
@@ -56,18 +71,21 @@ def _delete(args, data_dir):
         rmtree(dst)
     except FileNotFoundError:
         # TODO: asserts are striped in -0 so figure something else
-        assert False # THIS should not ever happen as it is checked
+        assert False  # THIS should not ever happen as it is checked
     except PermissionError:
         print("[red]Permission denied — folder may be partially deleted[/red]")
-
 
 
 def _add(args, data_dir):
     src = args.input_file
     dst = str(Path(data_dir) / Path(src).stem)
     try:
-        copytree(src, dst, dirs_exist_ok=True)
-    #TODO: figure these cases out
+        copytree(
+            src,
+            dst,
+            dirs_exist_ok=True,
+        )
+    # TODO: figure these cases out
     except FileNotFoundError:
         assert False
     except PermissionError:
@@ -82,23 +100,38 @@ def _create(args, data_dir):
     project_path = Path(args.project_name).resolve()
     template_name = args.type
 
-    if not _ensure_template_exists(args, data_dir,
-                                   template=template_name,
-                                   print_flag=True):
+    if not _ensure_template_exists(
+        args, data_dir, template=template_name, print_flag=True
+    ):
         return
 
     if project_path.exists():
-        if not Confirm.ask(f"'{project_path}' already exists. Overwrite?",
-                           default=False):
+        if not Confirm.ask(
+            f"'{project_path}' already exists. Overwrite?",
+            default=False,
+        ):
             print("[red]Aborted.[/red]")
             return
 
     src = str(Path(data_dir) / template_name)
     dst = str(project_path)
 
+    print(project_path.name)
+
     try:
-        copytree(src, dst, dirs_exist_ok=True)
-        print(f"[green]Project '{project_path.name}' created at {project_path.parent}[/green]")
+        copytree(
+            src,
+            dst,
+            dirs_exist_ok=True,
+            ignore=shutil.ignore_patterns(
+                ".git",
+            ),
+        )
+        replace_in_files(dst, {"<*project*>": project_path.name})
+
+        print(
+            f"[green]Project '{project_path.name}' created at {project_path.parent}[/green]"
+        )
     except PermissionError:
         print("[red]Permission denied[/red]")
     except shutil.Error as e:
@@ -109,7 +142,7 @@ def _create(args, data_dir):
 
 
 def _list(args, data_dir, print_flag=True):
-    
+
     _ = args
 
     templates = listdir(data_dir)
@@ -122,4 +155,3 @@ def _list(args, data_dir, print_flag=True):
             print(f"[orange1]{t}[/orange1]")
 
     return templates
-
